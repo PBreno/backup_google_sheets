c02f6a8647fa015485861d99fb0b86dffefe5e5b
PBreno <brenopombobp@hotmail.com>
2025-05-22 14:06:12 -0300

    script to be executed in windows

---
+++ script.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\breno.pombo\PycharmProjects\backup_google_sheets\src\public"
+"C:\Users\breno.pombo\PycharmProjects\backup_google_sheets\.venv\Scripts\python.exe" main.py
+exit
